5e6a21e93abb8549682d6c9aa20531e07f2e6abb
joselinares17 <josedaniellinares135@gmail.com>
2025-06-30 13:01:43 -0500

    adding initial services

---
+++ audit-service/src/test/java/org/pitch/audit_service/AuditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pitch.audit_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
